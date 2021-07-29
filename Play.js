76ee0bdfcd4d3772293bbcaa35a4e24be39c6418
chandni <chandnivaghasia11@gmail.com>
2021-07-29 11:40:06 -0400

    final commit

---
+++ Play.js
@@ -0,0 +1,10 @@
+const net = require("net");
+const connect = require('./client.js');
+const setupInput = require('./input.js');
+
+
+
+
+  console.log("Connecting ...");
+  let input = connect();
+  setupInput(input);
--- client.js
+++ client.js
@@ -1,28 +1,30 @@
 const net = require("net");
+const { IP, PORT } = require('./constants');
+
 const connect = function () {
   const conn = net.createConnection({
-    host: '135.23.223.133', // IP address here,
-    port: 50542, // PORT number here,
+    host: IP, // IP address here,
+    port: PORT, // PORT number here,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on('connect', () => {
     conn.Name = 'Name: JAN';
-    conn.write('Hello from client!');
+  
     conn.write(conn.Name);
+    
   });
   
   conn.on('data', (data) => {
-    
-   // console.log('Message from client: ', data)
-    //console.log("Successfully connected to game server");
+       console.log('Message from client: ', data)
+    console.log("Successfully connected to game server");
     conn.write("Move: up");
-    for (let i = 1; i < 20; i++) {
-      setTimeout(()=> {
-        conn.write('Move: up')
-      }, 500 * i)
-    } 
+    // for (let i = 1; i < 20; i++) {
+    //   setTimeout(()=> {
+    //     conn.write('Move: up')
+    //   }, 500 * i)
+    // } 
   });
 
   return conn;
