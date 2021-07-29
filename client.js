4458912e7ee0d07efc4db0658d332b0ac9ea5701
chandni <chandnivaghasia11@gmail.com>
2021-07-29 17:51:45 -0400

    changes

--- client.js
+++ client.js
@@ -31,4 +31,37 @@ const connect = function () {
 };
 
 
+module.exports = connect;const net = require("net");
+const { IP, PORT } = require('./constants');
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: IP, // IP address here,
+    port: PORT, // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on('connect', () => {
+    conn.Name = 'Name: JAN';
+  
+    conn.write(conn.Name);
+    
+  });
+  
+  conn.on('data', (data) => {
+       console.log('Message from client: ', data)
+    console.log("Successfully connected to game server");
+    conn.write("Move: up");
+    // for (let i = 1; i < 20; i++) {
+    //   setTimeout(()=> {
+    //     conn.write('Move: up')
+    //   }, 500 * i)
+    // } 
+  });
+
+  return conn;
+};
+
+
 module.exports = connect;
